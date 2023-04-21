13f1655076bdcd0f7fd2555ae5e4016b7575c318
Vlad <vladstefan8938@gmail.com>
2023-04-21 19:36:18 +0300

    Added function to check winner by comparing player's choice and computer's choice by looping through all options

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 console.log("Hello world");
 const choices = ["rock", "paper", "scissors"];
 
-function game () {
+function game() {
     // This is the main function
     // 5 Round 
     playRound();
@@ -14,12 +14,40 @@ function playRound() {
 
 function playerChoice() {
     // get input from player
-    let input = prompt("Type rock, paper, scissors: ");
+    let input = prompt("Type rock, paper or scissors: ");
+    while ( input == null) {
+        input = prompt("Type rock, paper or scissors: ");
+    }
     input = input.toLowerCase();
+    let check = validateInput(input);
+    while (check == false) {
+        input =prompt("Type rock, paper or scissors: ");
+        while ( input == null) {
+            input = prompt("Type rock, paper or scissors: ");
+        }
+        input = input.toLocaleLowerCase();
+        check = validateInput(input);
+    }
 }
 
 function computerChoice() {
-    return choices[Math.floor(Math.random()*choices.length)]
+    return choices[Math.floor(Math.random()*choices.length)];
+}
+
+function validateInput(choice) {
+    return choices.includes(choice);
+}
+
+// Function to check winner by comparing player's choice and computer's choice by looping through all options
+
+function checkWinner(choiceP, choiceC) {
+    if ((choiceP == 'rock' && choiceC == 'scissors') || (choiceP == 'paper'  && choiceC == 'rock') || (choiceP == 'scissors' && choiceC == 'paper')) {
+        return 'Player';
+    } else if ((choiceP == 'rock' && choiceC == 'paper') || (choiceP == 'paper' && choiceC == 'scissors') || (choiceP == 'scissors' && choiceC == 'rock')) {
+        return 'Computer';
+    } else {
+        return 'Tie';
+    }
 }
 
 game();
